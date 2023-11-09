2adcd7d421766a441bfc97bf76626032c2d0256f
LLF <21271209@bjtu.edu.cn>
2023-11-09 08:17:03 +0800

    Magic

---
+++ Lab03/socketpair.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+//#include <semaphore.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        cerr << "Usage: " << argv[0] << " <file_path> <word_to_search>" << std::endl;
+        return 1;
+    }
+
+    string file_path = argv[1];
+    string word_to_search = argv[2];
+    string file_out_path = argv[3];
+
+    int sockfd[2];
+    if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockfd) == -1) {
+        perror("socketpair");
+        return 1;
+    }else   cout << "socketpair success\n";
+
+
+
+    pid_t child_pid = fork();
+
+    if (child_pid == -1) {
+        perror("fork");
+        return 1;
+    }else if (child_pid == 0) { // Child process
+        cout << "child create success\n";
+
+//        fcntl(sockfd[1], F_SETFL, O_NONBLOCK);
+//        ofstream  fileout(file_out_path, ios::out | ios::binary);
+        FILE * sockread = fdopen(sockfd[1], "r+b");// 读写
+        if (sockread != nullptr) {
+            cout << "sockread success\n";
+            vector<string> stline;
+            char line[1024];
+            int i =0;
+            while (fgets(line, sizeof(line), sockread)) {
+                size_t len = strlen(line);
+
+                // 去除末尾的换行符
+                if (len > 0 && line[len - 1] == '\n') {
+                    line[len - 1] = '\0';
+                    len--;
+                }
+                if(*((int*)line)==0XDEADBEEF){
+                    cout << "--------------------------baibai================================\n" << endl;
+                    break;
+                }
+//                cout << i++ << endl;
+//                if(i >= 1) cout << (int)line[0] << "--" << line << "--"<<  endl;
+//                fileout <<i++<< line << endl;
+                string ll(line);
+
+                size_t pos = ll.find(argv[2]);
+                if (pos != string::npos) {
+                    bool should_add = true;
+                    if (isalpha(ll[pos - 1]) || isalpha(ll[pos + strlen(argv[2])])) {
+                        should_add = false;
+                    }
+                    if (should_add) {
+                        stline.push_back(ll);
+                    }
+                }
+            }
+            cout << "------------------------baibaiagain=========================="<<endl;
+
+//            for (auto& now : stline) {
+//                now +="\n";
+//                cout << now << endl;
+//            }
+
+            // 按字母表排序
+            sort(stline.begin(), stline.end());
+
+            for (auto& now : stline) {
+                now +="\n";
+                write(sockfd[1], now.c_str(), now.size());
+            }
+
+            int x = 0xDEADBEEF;
+            write(sockfd[1], &x, 4);
+            char  huanhang = '\n';
+            write(sockfd[1], &huanhang, 1);
+
+            cout << "---------------------------------成功================================" << endl;
+            close(sockfd[1]);
+            close(sockfd[0]);
+//            fileout.close();
+            exit(0);
+        }
+        exit(1);
+    } else if(child_pid > 0){ // Parent process
+        cout << "parent create success\n";
+
+        int buffer_size = 800000;  // 设置缓冲区大小，可以根据需要调整
+        if (setsockopt(sockfd[0], SOL_SOCKET, SO_RCVBUF, &buffer_size, sizeof(buffer_size)) == -1) {
+            perror("setsockopt");
+            return 1;
+        }
+
+//        fcntl(sockfd[0], F_SETFL, O_NONBLOCK);
+        ofstream fileout(file_out_path, ios::out | ios::binary);
+        ifstream file(file_path);
+        if (file.is_open()) {
+            string line;
+            while (getline(file, line)) {
+                line +="\n";
+                write(sockfd[0], line.c_str(), line.size());
+            }
+            int x = 0xDEADBEEF;
+            write(sockfd[0], &x, 4);
+            char  huanhang = '\n';
+            write(sockfd[0], &huanhang, 1);
+            file.close();
+        }
+
+
+        sleep(5);
+
+        FILE  * sock_readagain = fdopen(sockfd[0], "rb");
+        if(sock_readagain != nullptr){
+            char ans[1024];
+            while (fgets(ans, sizeof(ans), sock_readagain)) {
+                size_t len = strlen(ans);
+
+                // 去除末尾的换行符
+                if (len > 0 && ans[len - 1] == '\n') {
+                    ans[len - 1] = '\0';
+                    len--;
+                }
+                if(*((int*)ans)==0XDEADBEEF){
+                    cout << "--------------------------baibaiparentprocess================================\n" << endl;
+                    break;
+                }
+                fileout << ans << endl;
+            }
+            shutdown(sockfd[0], SHUT_WR);
+
+            fileout.close();
+        }
+        close(sockfd[0]);
+        close(sockfd[1]);
+        exit(0);
+
+    }
+
+    return 0;
+}
